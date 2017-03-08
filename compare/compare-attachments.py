4ade55c97877e8f78a8bb51e9b0c95efaefc5400
Jim Apple <jbapple-impala@apache.org>
2017-03-08 08:49:04 -0800

    Provide a way to override new and old server URLs in attachment comparison

--- compare/compare-attachments.py
+++ compare/compare-attachments.py
@@ -1,26 +1,11 @@
 #!/usr/bin/env python2
 
 # Checks issues on different servers for equal attachments.
-#
-# Usage:   ./compare-attachments.py IMPORTED_JSON [MINIMUM_ISSUE_KEY]
-# Example: ./compare-attachments.py 10-20.json    IMPALA-13
-# or
-#          ./compare-attachments.py MINIMUM_ISSUE_KEY MAXIMUM_ISSUE_KEY
-# Example: ./compare-attachments.py IMPALA-123        IMPALA-4567
-#
-# The first syntax reads all issues from 10-20.json, which should be the cleaned-up
-# exported json produced by add_missing_jira_fields.py, and compares all of the issues
-# with key greater than or equal to IMPALA-13. This requires fetching attachments from
-# both the source and destination JIRA system, but only reads the attachment metadata
-# (specifically, the URL) from the destination JIRA system.
-#
-# The second syntax reads attachment URLs from both the source and destination JIRA
-# systems, and so does not need a json export. It reads the issues with keys in
-# [IMPALA-123, IMPALA-4567) -- the MAXIMUM_ISSUE_KEY is not read.
-#
-# In both syntaxes, if an issue cannot be read, it is considered to have the "same"
-# attachments as an issue with no attachments.
 
+# If an issue cannot be read, it is considered to have the "same" attachments as an issue
+# with no attachments.
+
+import argparse
 import hashlib
 import json
 import urllib
@@ -89,17 +74,18 @@ def get_json_attachments(issue_dict):
 
 KEY_REGEX = re.compile("^.+-([1-9][0-9]*)$")
 
-def compare_export_attachments(json_export, minkey):
+def compare_export_attachments(json_export, minkey, maxkey, new_server):
   minkey_num = int(KEY_REGEX.match(minkey).group(1))
+  maxkey_num = int(KEY_REGEX.match(maxkey).group(1))
   for json_project in json_export["projects"]:
     for json_issue in json_project["issues"]:
       key = json_issue["key"]
       key_num = int(KEY_REGEX.match(key).group(1))
-      if key_num < minkey_num: continue
+      if key_num < minkey_num or key_num > maxkey_num: continue
       sys.stdout.write(key)
       sys.stdout.flush()
       from_json = get_json_attachments(json_issue)
-      from_xml = get_xml_attachments(key, "https://issues-test.apache.org/jira")
+      from_xml = get_xml_attachments(key, new_server)
       if len(from_json) == 0 == len(from_xml):
         print " NO-OP"
       elif from_json == from_xml:
@@ -123,23 +109,22 @@ def compare_xml_attachments(key, server1, server2):
     print attachments1.symmetric_difference(attachments2), "\n"
 
 if __name__ == "__main__":
-  if len(sys.argv) < 2 or sys.argv[1] in ["help", "-help", "--help", "-h"]:
-    print "Usage:  ", sys.argv[0], "IMPORTED_JSON [MINIMUM_ISSUE_KEY]"
-    print "Example:", sys.argv[0], "10-20.json    IMPALA-13"
-    print "or"
-    print "        ", sys.argv[0], "MINIMUM_ISSUE_KEY MAXIMUM_ISSUE_KEY"
-    print "Example:", sys.argv[0], "IMPALA-123        IMPALA-4567"
+  parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  old = parser.add_mutually_exclusive_group()
+  old.add_argument("--json", help="Filename of the JSON export")
+  old.add_argument("--old_server", default="https://issues.cloudera.org", help=" ")
+  parser.add_argument("--minkey", default="IMPALA-1", help=" ")
+  parser.add_argument("--maxkey", default="IMPALA-5999", help=" ")
+  parser.add_argument("--new_server", default="https://issues-test.apache.org/jira",
+                      help=" ")
+  args = parser.parse_args()
+  if args.json:
+    compare_export_attachments(
+        json.load(open(args.json)),
+        args.minkey, args.maxkey, args.new_server)
   else:
-    if len(sys.argv) == 2 or None is KEY_REGEX.match(sys.argv[1]):
-      assert len(sys.argv) <= 3
-      compare_export_attachments(json.load(open(sys.argv[1])),
-                                 sys.argv[2] if len(sys.argv) > 2 else "")
-    else:
-      assert len(sys.argv) == 3
-      start = int(KEY_REGEX.match(sys.argv[1]).group(1))
-      end   = int(KEY_REGEX.match(sys.argv[2]).group(1))
-      for key in xrange(start, end):
-        compare_xml_attachments("IMPALA-{}".format(key),
-                                "https://issues.cloudera.org",
-                                "https://issues-test.apache.org/jira")
-      print ""
+    start = int(KEY_REGEX.match(args.minkey).group(1))
+    end = int(KEY_REGEX.match(args.maxkey).group(1))
+    for key in xrange(start, end):
+      compare_xml_attachments("IMPALA-{}".format(key), args.old_server, args.new_server)
+    print ""
